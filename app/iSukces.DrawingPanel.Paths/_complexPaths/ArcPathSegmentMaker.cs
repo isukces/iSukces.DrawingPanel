23cab558e673dad888b2350d14f4646b72aa55e5
Piotr Stęclik <piotr.steclik@isukces.com>
2022-04-05 16:06:10 +0200

    fix some zero-reference path calculation

--- app/iSukces.DrawingPanel.Paths/_complexPaths/ArcPathSegmentMaker.cs
+++ app/iSukces.DrawingPanel.Paths/_complexPaths/ArcPathSegmentMaker.cs
@@ -21,11 +21,14 @@ namespace iSukces.DrawingPanel.Paths
                 var pathResult = ZeroReferencePointPathCalculator.Compute(_start, _end, Validator);
                 if (pathResult is null)
                 {
+                    return new ZeroReferencePointPathCalculatorLineResult(_end.Point, _end.Point);
+                    /*
                     var ex = new NotImplementedException(nameof(ZeroReferencePointPathCalculator) + " gives not result");
                     ex.Data.Set(nameof(_start), _start);
                     ex.Data.Set(nameof(_end), _end);
                     ex.Data.AddDebug();
                     throw ex;
+                */
                 }
 
                 return pathResult;
