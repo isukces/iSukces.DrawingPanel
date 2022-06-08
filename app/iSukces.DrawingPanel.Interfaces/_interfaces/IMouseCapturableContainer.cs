b4a49b323bec94f2d2d38a0bf12f6403c044d6f7
Piotr Stęclik <piotr.steclik@isukces.com>
2022-06-08 18:22:00 +0200

    adds IMouseCapturableContainer and simple implementation

---
+++ app/iSukces.DrawingPanel.Interfaces/_interfaces/IMouseCapturableContainer.cs
@@ -0,0 +1,10 @@
+using System.Windows.Forms;
+
+namespace iSukces.DrawingPanel.Interfaces
+{
+    public interface IMouseCapturableContainer
+    {
+        bool CaptureMouse(bool startCapture);
+
+    }
+}
